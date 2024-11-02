2bb0f2777660a66f030cca2ad94a79c0fb63a40f
imrany <imranmat254@gmail.com>
2024-11-02 12:42:15 +0300

    added textToSpeech function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function textToSpeech(text:string){
+    let audioState:any=localStorage.getItem("audio")
+    if(audioState==="unmute"||!audioState){
+        // Create a new instance of SpeechSynthesisUtterance
+        var speech = new SpeechSynthesisUtterance(text);
+
+        // Set the voice, rate, pitch, and language if desired
+        speech.lang = 'en-US'; // You can change the language
+        speech.rate = 1; // Speed (default is 1, range: 0.1 to 10)
+        speech.pitch = 1; // Pitch (default is 1, range: 0 to 2)
+
+        // Speak the text
+        window.speechSynthesis.speak(speech);
+    }else{
+        window.speechSynthesis.cancel()
+    }
+}
