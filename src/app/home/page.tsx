39caa56474bc08cff9b5d9fef691ba7bbaa2329e
imrany <imranmat254@gmail.com>
2024-11-17 17:27:14 +0300

    made it to read out the lastest response when sendPrompts is invoked

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -287,7 +287,10 @@ export default function Home() {
         setPrompts(parseRes.prompts);
         setTimeout(()=>{
           const id:string=parseRes.prompts[parseRes.prompts.length-1].id
+          const lastPrompt:string=parseRes.prompts[parseRes.prompts.length-1].prompt
+          const lastResponse:string=parseRes.prompts[parseRes.prompts.length-1].response
           scrollToBottom(id)
+          readAndStoreAudio(lastPrompt,lastResponse,id)
         },500)
       }
     } catch (error: any) {
